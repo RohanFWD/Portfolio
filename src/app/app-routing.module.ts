0f85eb6ec4008cb0321ffc0ee0ed3f0e98236ad7
rohan majumder <rohanmj561@gmail.com>
2020-06-25 23:10:47 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { PnfComponent } from './pnf/pnf.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',redirectTo: '/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'skill',component:SkillComponent},
+  {path:'education',component:EducationComponent},
+  {path:'project',component:ProjectComponent},
+  {path:'exp',component:ExperienceComponent},
+ {path:'contact',component:ContactComponent},
+ {path: "**" , component:PnfComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+
+import { ContactComponent } from './contact/contact.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { PnfComponent } from './pnf/pnf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
+  
+    ContactComponent,
+   
+    NavbarComponent,
+    HomeComponent,
+    SkillComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectComponent,
+    PnfComponent
   ],
   imports: [
     BrowserModule,
